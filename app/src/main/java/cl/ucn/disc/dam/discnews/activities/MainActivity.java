6f29cbfadc9037a0ff758ba236879659e199bbe7
John <jqm004@alumnos.ucn.cl>
2017-11-28 03:27:30 -0300

    Added: The articleDBFlowAdapter for adapting the articles on the BD

--- app/src/main/java/cl/ucn/disc/dam/discnews/activities/MainActivity.java
+++ app/src/main/java/cl/ucn/disc/dam/discnews/activities/MainActivity.java
@@ -12,17 +12,30 @@ import android.view.MenuItem;
 import android.widget.BaseAdapter;
 import android.widget.Toast;
 
+import com.squareup.seismic.ShakeDetector;
+
 import cl.ucn.disc.dam.discnews.R;
 
+import cl.ucn.disc.dam.discnews.tasks.GetSaveArticlesTask;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public final class MainActivity extends ListActivity {
+public final class MainActivity extends ListActivity implements GetSaveArticlesTask.TaskListener, ShakeDetector.Listener {
 
     /**
-     * Adapter de {@link cl.ucn.disc.dam.discnews.adapters.ArticleAdapter}.
+     * Adapter de {@link cl.ucn.disc.dam.discnews.model.Article).
      */
     private BaseAdapter articleAdapter;
+
+    /**
+     * Running background task
+     */
+    private GetSaveArticlesTask getSaveArticlesTask;
+
+    /**
+     * Shake Detector
+     */
+    private ShakeDetector shakeDetector;
 
     /**
      * @param savedInstanceState
@@ -46,12 +59,123 @@ public final class MainActivity extends ListActivity {
         this.getListView().setDivider(new GradientDrawable(GradientDrawable.Orientation.RIGHT_LEFT, colors));
         this.getListView().setDividerHeight(5);
 
-        // Si no hay articulos en el adaptador (y por lo tanto en la base de datos) ..
-        /*if (this.articleAdapter.isEmpty()) {
-            // .. ejecuto la tarea para obtenerlas.
-            //this.runGetAndSaveArticlesTask();
-        }*/
+        // Adaptador de articles
+        this.articleAdapter = new ArticleDBFlowAdapter(this);
+        super.setListAdapter(this.articleAdapter);
 
+        // Si no hay articulos en el adaptador (y por lo tanto en la base de datos) ..
+        if (this.articleAdapter.isEmpty()) {
+            // .. ejecuto la tarea para obtenerlas.
+            this.runGetAndSaveArticlesTask();
+        }
+
+        // Detector de terremotos
+        this.shakeDetector = new ShakeDetector(this);
+
+    }
+
+    /**
+     * Called after {@link #onCreate} &mdash; or after {@link #onRestart} when
+     * the activity had been stopped, but is now again being displayed to the
+     * user.  It will be followed by {@link #onResume}.
+     * <p>
+     * <p><em>Derived classes must call through to the super class's
+     * implementation of this method.  If they do not, an exception will be
+     * thrown.</em></p>
+     *
+     * @see #onCreate
+     * @see #onStop
+     * @see #onResume
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Al iniciar la aplicación, inicio la deteccion de terremotos
+        final SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        shakeDetector.start(sensorManager);
+
+        // Show the database size!
+        Toast.makeText(this, "Articles in BD: " + this.articleAdapter.getCount(), Toast.LENGTH_SHORT).show();
+
+    }
+
+    /**
+     * Called when you are no longer visible to the user.  You will next
+     * receive either {@link #onRestart}, {@link #onDestroy}, or nothing,
+     * depending on later user activity.
+     * <p>
+     * <p><em>Derived classes must call through to the super class's
+     * implementation of this method.  If they do not, an exception will be
+     * thrown.</em></p>
+     *
+     * @see #onRestart
+     * @see #onResume
+     * @see #onSaveInstanceState
+     * @see #onDestroy
+     */
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        // Detengo el detector de sismos
+        shakeDetector.stop();
+    }
+
+    /**
+     * Metodo que realiza la ejecucion en segundo plano de la tarea que obtiene los
+     * {@link com.durrutia.dnews.model.Article} desde Internet.
+     */
+    private void runGetAndSaveArticlesTask() {
+
+        // Si ya hay una tarea de obtencion de articulos corriendo no ejecuto una nueva!
+        if (this.getSaveArticlesTask != null) {
+            Toast.makeText(this, "Already downloading Articles ..", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Show little message
+        Toast.makeText(this, "Downloading Articles ..", Toast.LENGTH_LONG).show();
+
+        // Inicio la tarea
+        log.debug("Starting GetSaveArticlesTask ..");
+        this.getSaveArticlesTask = new GetSaveArticlesTask(this);
+        this.getSaveArticlesTask.execute();
+
+    }
+
+    /**
+     * Aviso que se termino la obtencion de los {@link com.durrutia.dnews.model.Article}.
+     *
+     * @param newsArticles numero de articulos nuevos obtenidos.
+     */
+    @Override
+    public void taskFinished(Integer newsArticles) {
+
+        // Show little message
+        Toast.makeText(this, "New Articles: " + newsArticles, Toast.LENGTH_LONG).show();
+
+        log.debug("Finished!");
+        this.articleAdapter.notifyDataSetChanged();
+
+        // Clean the task!
+        this.getSaveArticlesTask = null;
+
+    }
+
+    /**
+     * Called on the main thread when the device is shaken.
+     */
+    @Override
+    public void hearShake() {
+
+        // Vibro para indicar que se detecto el terremoto
+        Vibrator vibrator = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
+        if (vibrator != null) {
+            vibrator.vibrate(150);
+        }
+
+        this.runGetAndSaveArticlesTask();
     }
 
     /**
@@ -83,9 +207,10 @@ public final class MainActivity extends ListActivity {
     @Override
     public boolean onCreateOptionsMenu(final Menu menu) {
         final MenuInflater inflater = getMenuInflater();
-        //inflater.inflate(R.menu.main_manu, menu);
+        inflater.inflate(R.menu.main_manu, menu);
         return true;
     }
+
 
     /**
      * This hook is called whenever an item in your options menu is selected.
@@ -106,15 +231,14 @@ public final class MainActivity extends ListActivity {
     @Override
     public boolean onOptionsItemSelected(final MenuItem item) {
 
-        //switch (item.getItemId()) {
-        //    case R.id.menu_reload: {
-         //       this.runGetAndSaveArticlesTask();
+        switch (item.getItemId()) {
+            case R.id.menu_reload: {
+                this.runGetAndSaveArticlesTask();
                 return true;
-        //    }
-        //}
+            }
+        }
 
-        //return super.onOptionsItemSelected(item);
-
+        return super.onOptionsItemSelected(item);
 
     }
 
